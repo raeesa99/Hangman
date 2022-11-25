97289b99f7e5ab7958c8b15d2a56ab60f1199af2
raeesa99 <raeesapatel99@hotmail.com>
2022-11-25 12:21:27 +0000

    modified

--- hangmanfinal.js
+++ hangmanfinal.js
@@ -1,118 +1,7 @@
-// let topLineOfFrame = "_____________"
-// let topEdgeOfFrame = "|           |"
-// let frameBody = "|"
-// let baseOfFrame = "|___"
-// let head = "o" 
-// let body = "|"
-// let leftLimb = "/"
-// let rightLimb = "\\\ "
-// let loserMessage = "!! YOU LOSE !!"
-
-// let frame = (
-//     `${topLineOfFrame}
-// ${topEdgeOfFrame}
-// ${frameBody}
-// ${frameBody}
-// ${frameBody}
-// ${frameBody}
-// ${frameBody}
-// ${baseOfFrame}`
-// )
-
-// let framePlusHead = (
-//     `${topLineOfFrame}
-// ${topEdgeOfFrame}
-// ${frameBody}           ${head}
-// ${frameBody}
-// ${frameBody}
-// ${frameBody}
-// ${frameBody}
-// ${baseOfFrame}`
-//     )
-
-// let HeadBody = (
-//     `${topLineOfFrame}
-// ${topEdgeOfFrame}
-// ${frameBody}           ${head}
-// ${frameBody}           ${body}
-// ${frameBody}           ${body}
-// ${frameBody}
-// ${frameBody}
-// ${baseOfFrame}`
-//         )
-
-// let plusLeftarm = (
-//     `${topLineOfFrame}
-// ${topEdgeOfFrame}
-// ${frameBody}           ${head}
-// ${frameBody}           ${body}
-// ${frameBody}          ${leftLimb}${body}
-// ${frameBody}
-// ${frameBody}
-// ${baseOfFrame}`
-//     )
-
-// let plusBothArms = (
-//      `${topLineOfFrame}
-// ${topEdgeOfFrame}
-// ${frameBody}           ${head}
-// ${frameBody}           ${body}
-// ${frameBody}          ${leftLimb}${body}${rightLimb}
-// ${frameBody}
-// ${frameBody}
-// ${baseOfFrame}`
-//     )
-
-// let plusLeftleg = (
-//         `${topLineOfFrame}
-// ${topEdgeOfFrame}
-// ${frameBody}           ${head}
-// ${frameBody}           ${body}
-// ${frameBody}          ${leftLimb}${body}${rightLimb}
-// ${frameBody}          ${leftLimb}
-// ${frameBody}
-// ${baseOfFrame}`
-// )
-
-// let loser = (
-//     `${topLineOfFrame}
-// ${topEdgeOfFrame}
-// ${frameBody}           ${head}
-// ${frameBody}           ${body}
-// ${frameBody}          ${leftLimb}${body}${rightLimb}
-// ${frameBody}          ${leftLimb} ${rightLimb}
-// ${frameBody}
-// ${baseOfFrame}
-// ${loserMessage}`  
-// )
-
-// const hangman = [`${frame}`, `${framePlusHead}`, `${HeadBody}`, `${plusLeftarm}`, `${plusBothArms}`, `${plusLeftleg}`, `${loser}`]
-// //i need to correlate the guesses to every time maxGuesses decreases by 1
-
-// let words = ["mouse", "clean", "style", "fleets", "teepee", "close", "achoo", "doubt", "shelf"]
-//a
-
 generateWord = (words) => {
     let random = Math.floor(Math.random() * words.length);
     return words[random];
 }
-// let wordToGuess = generateWord(words)
-
-// let blankLetter = '_'
-// let blankWordToGuess = [ ]
-// for (let i = 0; i < wordToGuess.length; i++) {
-//     blankWordToGuess.push(blankLetter)
-// };
-// let correctGuesses = blankWordToGuess.fill(blankLetter)
-
-
-// let maxGuesses = 6
-// let guessedLetters = [ ]
-
-// console.log(frame)
-// console.log(blankWordToGuess)
-// console.log(maxGuesses + " guesses remaining")
-// // console.log(wordToGuess)
 
 const printInfo = (correctGuesses, maxGuesses, guessedLetters, hangman) => {  
     console.log(hangman)
@@ -120,42 +9,6 @@ const printInfo = (correctGuesses, maxGuesses, guessedLetters, hangman) => {
     console.log('Guesses remaining: ' + maxGuesses);
     console.log('Incorrect letters: ' + guessedLetters.join(''));
 }
-
-// //loop starts here
-// const game = () => {
-//     while (maxGuesses > 0) {
-
-//         let guess = prompt("take a guess");
-
-//         let correctGuess = false;   
-
-//         for (let i = 0; i < wordToGuess.length; i++) {
-//                 if (wordToGuess[i] === guess) {
-//                     correctAttempts[i] = guess + ' ';
-//                     correctGuess = true;
-//                 }             
-//             }
-
-//             guessedLetters.push(guess);
-
-//             if (!correctGuess) {
-//                 maxGuesses--;
-//                 //console.log(hangman)
-//             }
-//         }
-//     }
-
-//     game()
-
-// const guess = prompt("take a guess") //i can start my game loop with this prompt
-
-// const pushGuess = (guess) => { 
-// for (let i = 0; i < blankWordToGuess.length; i++) {
-//     if (blankWordToGuess[i] === guess) {
-//         blankWordToGuess.splice(blankWordToGuess.indexOf(i), 1, [j])
-//     }
-//   }
-// }
 
 const checkValidUserInput = (guess, wordToGuess, guessedLetters, maxGuesses, correctGuesses, hangman) => {
     let correctGuess = false;
@@ -186,7 +39,6 @@ const checkGameOver = (correctGuesses, maxGuesses, wordToGuess) => {
 
     return false;
 }
-
 
 const game = () => {
     const words = ["mouse", "clean", "style", "fleets", "teepee", "close", "achoo", "doubt", "shelf"]
