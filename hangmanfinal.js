3e96d714bd66f70916fe44909efef36430bbdd23
raeesa99 <raeesapatel99@hotmail.com>
2022-11-25 22:17:40 +0000

    modified

--- hangmanfinal.js
+++ hangmanfinal.js
@@ -3,37 +3,61 @@ generateWord = (words) => {
     return words[random];
 }
 
-const printInfo = (correctGuesses, maxGuesses, guessedLetters, hangman) => {  
-    console.log(hangman)
+const printInfo = (correctGuesses, maxGuesses, guessedLetters, bp) => {  
+    console.log(' |‾‾‾‾‾|');
+    console.log(` ${bp[0]}     |`);
+    console.log(`${bp[2]}${bp[1]}${bp[3]}    |`);
+    console.log(` ${bp[4]}${bp[5]}    |`);
+    console.log(' ‾‾‾‾‾‾');
+    console.log('\n')
+    
     console.log(correctGuesses.join(''));
-    console.log('Guesses remaining: ' + maxGuesses);
+    console.log('Attempts remaining: ' + maxGuesses);
     console.log('Incorrect letters: ' + guessedLetters.join(''));
 }
 
-const checkValidUserInput = (guess, wordToGuess, guessedLetters, maxGuesses, correctGuesses, hangman) => {
+const checkUserInput = (userInput, correctGuesses, guessedLetters) => {
+    if (!userInput) {
+        alert('Please enter a letter');
+        return false;
+    } else if(userInput.length > 1) {
+        alert('Only enter a single letter at a time');
+        return false;
+    } else if(correctGuesses.includes(userInput + ' ') || guessedLetters.includes(userInput + ' ')) {
+        alert('You have already tried: ' + userInput);
+        return false;
+    }
+
+    return true;
+}
+
+const checkValidUserInput = (userInput, wordToGuess, correctGuesses, maxGuesses, guessedLetters, all, bp) => {
     let correctGuess = false;
 
-        for (let i = 0; i < wordToGuess.length; i++) {
-            if (wordToGuess[i] === guess) {
-                 correctGuesses[i] = guess + ' ';
-                 correctGuess = true;
-            }
-          } 
-          if (!correctGuess) {
-        maxGuesses -- ;
-        guessedLetters.push(guess + ' ');
-  } 
-  return printInfo;
+    for (let i = 0; i < wordToGuess.length; i++) {
+        if(wordToGuess[i] === userInput) {
+            correctGuesses[i] = userInput + ' ';
+            correctGuess = true;
+        }
+    }
+
+    if (!correctGuess) {
+        maxGuesses--;
+        guessedLetters.push(userInput + ' ');
+        bp[5 - maxGuesses] = all[5 - maxGuesses]
+    }
+
+    return {bp: bp, maxGuesses: maxGuesses};
 }
 
 const checkGameOver = (correctGuesses, maxGuesses, wordToGuess) => {
-    let attempts = correctGuesses.map(letter => letter[0])
+    let thisAttempt = correctGuesses.map(letter => letter[0])
 
     if (maxGuesses == 0) {
-        alert('Uh-oh...You lose');
+        alert('Uh-Oh, better luck next time');
         return true;
-    } else if(attempts.join('') === wordToGuess) {
-        alert("Congrats...You've won!");
+    } else if(thisAttempt.join('') === wordToGuess) {
+        alert(`Congrats! You guessed the full word:  ` + wordToGuess);
         return true;
     }
 
@@ -41,33 +65,36 @@ const checkGameOver = (correctGuesses, maxGuesses, wordToGuess) => {
 }
 
 const game = () => {
-    const words = ["mouse", "clean", "style", "fleets", "teepee", "close", "achoo", "doubt", "shelf"]
+    const words = ["mouse", "clean", "style", "fleets", "teepee", "close", "achoo", "doubt", "shelf"];
     let wordToGuess = generateWord(words);
     let maxGuesses = 6;
-    let correctGuesses = new Array(wordToGuess.length).fill('_ '); //JAZZ =  _ _ _ _ 
+    let correctGuesses = new Array(wordToGuess.length).fill('_ '); 
     let guessedLetters = [];
     let all = ['o', '|', '-', '-', '/', '\\'];
     let bp = [' ', ' ', ' ', ' ', ' ', ' '];
-;
+
     let name = prompt("Hi there, what is your name?");
     alert(`Welcome to hangman ${name}`);
     while(maxGuesses > 0) {
-        printInfo(correctGuesses, maxGuesses, guessedLetters, hangman);
-        let guess = prompt("Please enter a letter");
+        printInfo(correctGuesses, maxGuesses, guessedLetters, bp);
+        console.log(maxGuesses);
+        let userInput = prompt("Please enter a letter").toLowerCase();
 
-        if (!checkValidUserInput(guess, wordToGuess, guessedLetters, maxGuesses, correctGuesses, hangman)) {
+        if (!checkUserInput(userInput, correctGuesses, guessedLetters)) {
             continue;
         }
 
-        let output = checkValidUserInput(guess, wordToGuess, guessedLetters, maxGuesses, correctGuesses, hangman);
+        let output = checkValidUserInput(userInput, wordToGuess, correctGuesses, maxGuesses, guessedLetters, all, bp);
+        
         maxGuesses = output.maxGuesses;
+        bp = output.bp
 
         if(checkGameOver(correctGuesses, maxGuesses, wordToGuess)) {
-            printInfo(correctGuesses, maxGuesses, guessedLetters, hangman);
+            printInfo(correctGuesses, maxGuesses, guessedLetters, bp);
             return;
         }
     }
 }
-game();
 
+game();
 
